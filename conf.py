3e4e43433830920af035675d1d9469a6d3918ec7
Philipp A <flying-sheep@web.de>
2023-08-17 16:23:00 +0200

    switch to book theme (#75)

--- conf.py
+++ conf.py
@@ -1,11 +1,14 @@
 from datetime import datetime
+from pathlib import Path
+from importlib.metadata import metadata
 
-project = 'scanpy-tutorials'
-author = 'Alex Wolf, Fidel Ramirez, Sergei Rybakov'
+from nbclean import NotebookCleaner
+
+meta = metadata('scanpy-tutorials')
+project = meta['Name']
+author = meta['Author']
 copyright = f'{datetime.now():%Y}, {author}'
-
-version = ''
-release = version
+release = version = meta['Version']
 
 extensions = [
     'nbsphinx',
@@ -14,35 +17,24 @@ extensions = [
 templates_path = ['_templates']
 source_suffix = '.rst'
 master_doc = 'index'
-language = None
+language = 'en'
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints', 'scanpy_workshop/*']
 pygments_style = 'sphinx'
 
 # -- Options for HTML output ----------------------------------------------
 
-html_theme = 'sphinx_rtd_theme'
-html_theme_options = dict(
-    navigation_depth=4, logo_only=True  # Only show the logo
-)
-html_context = dict(
-    display_github=True,      # Integrate GitHub
-    github_user='theislab',   # Username
-    github_repo='scanpy-tutorials',     # Repo name
-    github_version='master',  # Version
-    conf_py_path='/',    # Path in the checkout to the docs root
+html_theme = 'sphinx_book_theme'
+html_theme_options = dict( 
+    repository_url='https://github.com/theislab/scanpy-tutorials',
+    repository_branch='master',
+    use_repository_button=True,
 )
 html_static_path = ['_static']
-html_show_sphinx = False
 html_logo = '_static/img/Scanpy_Logo_BrightFG.svg'
-
-def setup(app):
-    app.add_stylesheet('css/custom.css')
 
 # -- Strip output ----------------------------------------------
 
-import nbclean, glob
-
-for filename in glob.glob('**/*.ipynb', recursive=True):
-    ntbk = nbclean.NotebookCleaner(filename)
+for path in Path().rglob('**/*.ipynb'):
+    ntbk = NotebookCleaner(str(path))
     ntbk.clear('stderr')
-    ntbk.save(filename)
+    ntbk.save(path)
