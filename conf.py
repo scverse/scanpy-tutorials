74e570c68511e7226033061c64283343f3d92fdb
Philipp A. <flying-sheep@web.de>
2024-02-27 10:58:34 +0100

    Add formatting

--- conf.py
+++ conf.py
@@ -4,37 +4,43 @@ from importlib.metadata import metadata
 
 from nbclean import NotebookCleaner
 
-meta = metadata('scanpy-tutorials')
-project = meta['Name']
-author = meta['Author']
-copyright = f'{datetime.now():%Y}, {author}'
-release = version = meta['Version']
+meta = metadata("scanpy-tutorials")
+project = meta["Name"]
+author = meta["Author"]
+copyright = f"{datetime.now():%Y}, {author}"
+release = version = meta["Version"]
 
 extensions = [
-    'nbsphinx',
+    "nbsphinx",
 ]
 
-templates_path = ['_templates']
-source_suffix = '.rst'
-master_doc = 'index'
-language = 'en'
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints', 'scanpy_workshop/*']
-pygments_style = 'sphinx'
+templates_path = ["_templates"]
+source_suffix = ".rst"
+master_doc = "index"
+language = "en"
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+    "**.ipynb_checkpoints",
+    "scanpy_workshop/*",
+]
+pygments_style = "sphinx"
 
 # -- Options for HTML output ----------------------------------------------
 
-html_theme = 'sphinx_book_theme'
-html_theme_options = dict( 
-    repository_url='https://github.com/theislab/scanpy-tutorials',
-    repository_branch='master',
+html_theme = "sphinx_book_theme"
+html_theme_options = dict(
+    repository_url="https://github.com/theislab/scanpy-tutorials",
+    repository_branch="master",
     use_repository_button=True,
 )
-html_static_path = ['_static']
-html_logo = '_static/img/Scanpy_Logo_BrightFG.svg'
+html_static_path = ["_static"]
+html_logo = "_static/img/Scanpy_Logo_BrightFG.svg"
 
 # -- Strip output ----------------------------------------------
 
-for path in Path().rglob('**/*.ipynb'):
+for path in Path().rglob("**/*.ipynb"):
     ntbk = NotebookCleaner(str(path))
-    ntbk.clear('stderr')
+    ntbk.clear("stderr")
     ntbk.save(path)
