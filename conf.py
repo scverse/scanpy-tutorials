ef9c38fccd8db0d87e423bfb4646d98d9cc325ad
Philipp A <flying-sheep@web.de>
2024-03-11 19:35:32 +0100

    Switch to myst-nb (#86)
    * Switch to myst-nb
    
    * missed one
    
    * use theme
    
    * 3.12
    
    * don’t execute
    
    * monkeypatch
    
    * Add docstring explaining custom `render_image`
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: Isaac Virshup <ivirshup@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- conf.py
+++ conf.py
@@ -1,8 +1,9 @@
 from datetime import datetime
-from pathlib import Path
 from importlib.metadata import metadata
 
-from nbclean import NotebookCleaner
+from sphinx.application import Sphinx
+from myst_nb.core.render import NbElementRenderer, MimeData
+from docutils import nodes
 
 meta = metadata("scanpy-tutorials")
 project = meta["Name"]
@@ -11,7 +12,10 @@ copyright = f"{datetime.now():%Y}, {author}"
 release = version = meta["Version"]
 
 extensions = [
-    "nbsphinx",
+    "myst_nb",
+]
+myst_enable_extensions = [
+    "colon_fence",
 ]
 
 templates_path = ["_templates"]
@@ -29,7 +33,7 @@ pygments_style = "sphinx"
 
 # -- Options for HTML output ----------------------------------------------
 
-html_theme = "sphinx_book_theme"
+html_theme = "scanpydoc"
 html_theme_options = dict(
     repository_url="https://github.com/theislab/scanpy-tutorials",
     repository_branch="master",
@@ -38,9 +42,36 @@ html_theme_options = dict(
 html_static_path = ["_static"]
 html_logo = "_static/img/Scanpy_Logo_BrightFG.svg"
 
-# -- Strip output ----------------------------------------------
+# -- Notebook settings ----------------------------------------------------
 
-for path in Path().rglob("**/*.ipynb"):
-    ntbk = NotebookCleaner(str(path))
-    ntbk.clear("stderr")
-    ntbk.save(path)
+nb_execution_mode = "off"
+nb_output_stderr = "remove"
+myst_heading_anchors = 3
+
+render_image_orig = NbElementRenderer.render_image
+
+
+def render_image(self: NbElementRenderer, data: MimeData) -> list[nodes.Element]:
+    """Makes images display size default to their jupyter setting.
+
+    Workaround for: https://github.com/executablebooks/MyST-NB/issues/522
+    """
+    node_list = render_image_orig(self, data)
+    try:
+        [image] = node_list
+        if not isinstance(image, nodes.image):
+            raise ValueError(f"Expected nodes.image, got {type(image)}")
+        for key in ("width", "height"):
+            if key in image:
+                continue
+            if (v := data.output_metadata.get(data.mime_type, {}).get(key)) is None:
+                continue
+            image[key] = str(v)
+    except Exception:
+        return node_list
+    return [image]
+
+
+def setup(app: Sphinx) -> None:
+    if NbElementRenderer.render_image is render_image_orig:
+        NbElementRenderer.render_image = render_image
