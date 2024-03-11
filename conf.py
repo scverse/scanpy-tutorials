d8249b5a600e883c8fd19b1aa2f04febf7b142e9
Phil Schaf <flying-sheep@web.de>
2024-03-11 13:47:50 +0100

    Switch to myst-nb

--- conf.py
+++ conf.py
@@ -1,8 +1,5 @@
 from datetime import datetime
-from pathlib import Path
 from importlib.metadata import metadata
-
-from nbclean import NotebookCleaner
 
 meta = metadata("scanpy-tutorials")
 project = meta["Name"]
@@ -11,7 +8,10 @@ copyright = f"{datetime.now():%Y}, {author}"
 release = version = meta["Version"]
 
 extensions = [
-    "nbsphinx",
+    "myst_nb",
+]
+myst_enable_extensions = [
+    "colon_fence",
 ]
 
 templates_path = ["_templates"]
@@ -29,7 +29,7 @@ pygments_style = "sphinx"
 
 # -- Options for HTML output ----------------------------------------------
 
-html_theme = "sphinx_book_theme"
+html_theme = "scanpydoc"
 html_theme_options = dict(
     repository_url="https://github.com/theislab/scanpy-tutorials",
     repository_branch="master",
@@ -38,9 +38,7 @@ html_theme_options = dict(
 html_static_path = ["_static"]
 html_logo = "_static/img/Scanpy_Logo_BrightFG.svg"
 
-# -- Strip output ----------------------------------------------
+# -- Notebook settings ----------------------------------------------------
 
-for path in Path().rglob("**/*.ipynb"):
-    ntbk = NotebookCleaner(str(path))
-    ntbk.clear("stderr")
-    ntbk.save(path)
+nb_output_stderr = "remove"
+myst_heading_anchors = 3
