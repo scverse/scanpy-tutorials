f4a80acdcec734c7cb4cde5cfc74f2c1d47e2d2f
Philipp A. <flying-sheep@web.de>
2024-06-24 17:39:25 +0200

    WIP canonical URL banner

--- conf.py
+++ conf.py
@@ -1,9 +1,19 @@
+from collections.abc import Mapping
 from datetime import datetime
 from importlib.metadata import metadata
+from types import MappingProxyType
+from typing import TYPE_CHECKING, Sequence, cast
 
-from sphinx.application import Sphinx
-from myst_nb.core.render import NbElementRenderer, MimeData
 from docutils import nodes
+from sphinx import addnodes
+from sphinx.domains import Domain
+from sphinx.environment import BuildEnvironment
+from sphinx.ext.intersphinx import resolve_reference_in_inventory
+
+if TYPE_CHECKING:
+    from docutils.parsers.rst.states import Inliner
+    from sphinx.application import Sphinx
+
 
 meta = metadata("scanpy-tutorials")
 project = meta["Name"]
@@ -13,6 +23,7 @@ release = version = meta["Version"]
 
 extensions = [
     "myst_nb",
+    "sphinx.ext.intersphinx",
 ]
 myst_enable_extensions = [
     "colon_fence",
@@ -31,6 +42,13 @@ exclude_patterns = [
 ]
 pygments_style = "sphinx"
 
+intersphinx_mapping = dict(
+    scanpy=("https://scanpy.readthedocs.io/en/stable/", None),
+)
+intersphinx_disabled_reftypes = []
+# TODO: move images here from scanpy
+suppress_warnings = ["image.not_readable"]
+
 # -- Options for HTML output ----------------------------------------------
 
 html_theme = "scanpydoc"
@@ -48,30 +66,53 @@ nb_execution_mode = "off"
 nb_output_stderr = "remove"
 myst_heading_anchors = 3
 
-render_image_orig = NbElementRenderer.render_image
+
+# Roles “implementing” {cite}`…` and {cite:p}`…`/{cite:t}`…`
 
 
-def render_image(self: NbElementRenderer, data: MimeData) -> list[nodes.Element]:
-    """Makes images display size default to their jupyter setting.
+def fake_cite(
+    name: str,
+    rawtext: str,
+    text: str,
+    lineno: int,
+    inliner: Inliner,
+    options: Mapping[str, object] = MappingProxyType({}),
+    content: Sequence[str] = (),
+) -> tuple[list[nodes.Node], list[str]]:
+    node = inliner.document.reporter.info(f"cite:{text}")
+    return [node], []
 
-    Workaround for: https://github.com/executablebooks/MyST-NB/issues/522
-    """
-    node_list = render_image_orig(self, data)
-    try:
-        [image] = node_list
-        if not isinstance(image, nodes.image):
-            raise ValueError(f"Expected nodes.image, got {type(image)}")
-        for key in ("width", "height"):
-            if key in image:
-                continue
-            if (v := data.output_metadata.get(data.mime_type, {}).get(key)) is None:
-                continue
-            image[key] = str(v)
-    except Exception:
-        return node_list
-    return [image]
+
+class FakeDomain(Domain):
+    name = "cite"
+    roles = dict(p=fake_cite, t=fake_cite)
+
+
+# Role linking to the canonical location in scanpy’s docs
+
+
+def canonical_tutorial(
+    name: str,
+    rawtext: str,
+    text: str,
+    lineno: int,
+    inliner: Inliner,
+    options: Mapping[str, object] = MappingProxyType({}),
+    content: Sequence[str] = (),
+) -> tuple[list[nodes.Node], list[str]]:
+    env = cast(BuildEnvironment, inliner.document.settings.env)
+    # TODO: do a big banner thing here
+    node = resolve_reference_in_inventory(
+        env,
+        "scanpy",
+        addnodes.pending_xref(text, reftype="doc", refdomain="std", reftarget=text),
+        nodes.inline(rawtext, text),
+    )
+    assert node
+    return [node], []
 
 
 def setup(app: Sphinx) -> None:
-    if NbElementRenderer.render_image is render_image_orig:
-        NbElementRenderer.render_image = render_image
+    app.add_domain(FakeDomain)
+    app.add_role("cite", fake_cite)
+    app.add_role("canonical-tutorial", canonical_tutorial)
--- index.rst
+++ index.rst
@@ -4,12 +4,16 @@
 .. toctree::
    :maxdepth: 1
 
+   basic-scrna-tutorial
    pbmc3k
    paga-paul15
    plotting/core
    plotting/advanced
+   plotting-with-marsilea
    integrating-data-using-ingest
+   dask
    knn-transformers
+   tutorial_pearson_residuals
    spatial/basic-analysis
    spatial/integration-scanorama
 ..   cite-seq/pbmc5k
